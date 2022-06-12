7b2ea7161641806e9b82cf31e99460fab53485a2
aryanaren <aryanaren3@gmail.com>
2022-06-12 17:16:46 -0500

    week1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_image from "../assets/profile_image.png";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          src={profile_image}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Arya Naren</div>
+        <div className="brief_description">
+          Rising Junior @ Virginia Tech | Intern @ Visa 
+        </div>
+      </div>
+    </div>
       </div>
     )
   }
